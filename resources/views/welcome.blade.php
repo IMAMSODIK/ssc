8e4c12cd9dbe1c89ddb145e45113e963ea1041ef
IMAM SODIK <imamsodik00@gmail.com>
2025-08-08 23:13:40 +0700

    asdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
                 <li><a href="#home" class="active">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#service">Services</a></li>
-                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="#gallery">Gallery</a></li>
                 <li><a href="#attonery">Attorneys</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
